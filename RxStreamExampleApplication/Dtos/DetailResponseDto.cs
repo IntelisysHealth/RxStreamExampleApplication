d15dba870620543a408a63f19985c2eae86104e0
Allen Crane <--list>
2018-03-19 14:15:42 -0500

    Adding Npi and Ncpdp to DTO

--- RxStreamExampleApplication/Dtos/DetailResponseDto.cs
+++ RxStreamExampleApplication/Dtos/DetailResponseDto.cs
@@ -8,7 +8,9 @@ namespace RxStreamExampleApplication.Dtos
 {
     public class DetailResponseDto
     {
-        public string PharmacyId { get; set; }
+        public string Npi { get; set; }
+        public string Ncpdp { get; set; }
+        public string Status { get; set; }
         public string PrescriptionNumber { get; set; }
         public string Cost { get; set; }
     }
